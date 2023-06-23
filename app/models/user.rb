187708fe5d2190622dcf10f16bc5e9f7ba6f6486
Dustin Doyle <126806607+Dantexkilljoy@users.noreply.github.com>
2023-06-23 20:16:27 +0000

    add direct and indirect associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,30 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :comments, foreign_key: "author_id", dependent: :destroy
+
+  has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest", dependent: :destroy
+
+  has_many :accepted_sent_follow_requests, -> { accepted }, foreign_key: :sender_id, class_name: "FollowRequest"
+
+  has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest", dependent: :destroy
+
+  has_many :accepted_received_follow_requests, -> { accepted }, foreign_key: :recipient_id, class_name: "FollowRequest", dependent: :destroy
+  
+  has_many :likes, class_name: "Like", foreign_key: :fan_id, dependent: :destroy 
+  
+  has_many :own_photos, class_name: "Photo", foreign_key: "owner_id", dependent: :destroy
+  
+  has_many :liked_photos, through: :likes, source: :photo, dependent: :destroy
+
+  has_many :leaders, through: :accepted_sent_follow_requests, source: :recipient
+
+  has_many :followers, through: :accepted_received_follow_requests, source: :sender
+
+  has_many :feed, through: :leaders, source: :own_photos
+
+  has_many :discover, through: :leaders, source: :liked_photos
+
+  validates :username, presence: true, uniqueness: true
 end
